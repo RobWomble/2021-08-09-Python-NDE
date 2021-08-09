9994a85bb7aa93c98a12c32ddeac9a26715336c4
sgriffith3 <sgriffith@alta3.com>
2021-08-09 19:48:09 +0000

    challenge

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hi Steve!")
+
+age = 5
+name = "mudge"
+color = "copper"
+
+
+
+print("My dog", name, "is", age, "and is a", color, "color.", sep=" *** ", end=" :-)\n", file=open("myprintedinfo", "w"))
+
+
